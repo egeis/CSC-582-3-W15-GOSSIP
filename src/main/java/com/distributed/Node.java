cf32da548f94679d59e70bf8a2b93b9f3783728b
egeis <rcoan@callutheran.edu>
2015-02-18 02:38:49 -0800

    Altered shutdown in parsePacket, Reverted the parsePacket loop back. Remaining variables / methods are all now static types. Re-implemented the logger formatting. ---(Multiple files with numbers added depending on if a thread attempts to write a log at the same time the file is being used.) ---We could use the Apache Logger if we want to use a buffered logger but id rather not add more libraries than necessary. Fixed an issue with sendPacket type conversion error with the "keys[i]" had to enable verbose output during the build to track it down since the logic was legal.... commented out the try - statement to suppress an the io error since the catch is not in use. The "adjacent" hashmap contains the nodes neighbors.

--- src/main/java/com/distributed/Node.java
+++ src/main/java/com/distributed/Node.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package main.java.com.distributed;
 
 import java.io.DataInputStream;
@@ -12,9 +7,11 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.Objects;
+import java.util.Random;
 import java.util.Set;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -29,7 +26,7 @@ import main.java.com.distributed.network.PacketHelper;
 
 /**
  *
- * @author Richard Coan
+ * @author Richard Coan & Matt Chung
  */
 public class Node {
     private static ConcurrentHashMap<String, Node.Values> data = new ConcurrentHashMap(16, 0.9f, 1);
@@ -39,19 +36,15 @@ public class Node {
     
     private static ServerSocket server;
     private static ObjectInputStream input;
-    private ObjectOutputStream output;
-    private boolean shutdown = false;
+    private static ObjectOutputStream output;
+    private static boolean shutdown = false;
     
-    public static Integer parent = null;
-    private Integer id = null;
+    private static Integer id = null;
     private static int port = -1;
-    private String host = "";
-    private Boolean isInit = false;
+    private static String host = "";
+    private static Boolean isInit = false;
     
-    //private static final Map<Integer,Conn.Child> children = new HashMap();
-    
-    //private static final Map<Integer,Boolean> incoming = new HashMap();
-    //private static final Map<Integer,Boolean> outgoing = new HashMap();
+    private static final Map<Integer,Conn.Child> adjacent = new HashMap();
     
     public static String TIMER_HOST = "localhost";
     public static int TIMER_PORT = 1212;
@@ -60,16 +53,11 @@ public class Node {
     private static int Mn = 0;
     private static int N = 0;
     
-    private static int[] neighbors;
-            
-    public Node(Integer id, String host, int port)
-    {        
-        this.id = id;
-        this.host = host;
-        this.port = port;
-    }
-    
-    private long RequestNewUpdateTime()
+    /**
+     * Requests the next counter from a global counter server.
+     * @return a unique number [LONG].
+     */
+    private long RequestUpdateTime()
     {
         long time = 0;
         try ( 
@@ -87,11 +75,9 @@ public class Node {
         
         return time;
     }
-    
+
     private static void parsePacket(Packet p)
-    {        
-        //Packet send;
-        
+    {                
         LOGGER.info("Recieved from node_"+p.id+" Packet:"+p.toString());
         
         switch(p.type) 
@@ -106,13 +92,9 @@ public class Node {
                 start();
                 
                 break;
-//            case PacketHelper.INIT_INITIATOR:
-//                isInit = true;
-//                break;
             case PacketHelper.INIT_SHUTDOWN:
-                //LOGGER.info(incoming.toString()+System.getProperty("line.separator")+outgoing.toString());
-                //shutdown = true;
-                //break;
+                shutdown = true;
+                break;
             default:
         }
     }
@@ -150,23 +132,22 @@ public class Node {
         
         return p;
     }
-    
+
     /**
-     * Sends a packet of data to the initiator.
-     * @param p the packet.
-     * @param the type of send, 0 for parent, 1 for children (excluding parent)
-     * 2 for all.
+     * Sends a packet into the great unknown where it will meet an unknown fate
+     * in this cruel digital world.
+     * @param value 
      */
-    private static void sendPacket(String key, Values value)
+    private static void sendPacket(Values value)
     {        
-        try 
-        {
-            Packet p = PacketHelper.getPacket(PacketHelper.MESSAGE, value);
-            
-            
-        } catch (IOException ex) {
-           LOGGER.log(Level.SEVERE, ex.getMessage(), ex);
-        }
+        Packet p = PacketHelper.getPacket(PacketHelper.MESSAGE, id, value);
+        
+//        try 
+//        {
+//            
+//        } catch (IOException ex) {
+//           LOGGER.log(Level.SEVERE, ex.getMessage(), ex);
+//        }
     }
     
     public static long getTime()
@@ -211,8 +192,8 @@ public class Node {
     
     public static void main(String[] args)
     {
-        int port = 0;
-        int id = 0;
+        port = 0;
+        id = 0;
         String host = "";
         
         if(args.length > 1 )
@@ -226,70 +207,60 @@ public class Node {
             id = Integer.parseInt(parts[0]);
             host = parts[1];
             port = Integer.parseInt(parts[2]);
-            LOGGER = Logger.getLogger(Node.class.getName()+"_"+id+"_"+host+"-"+port);
-            
-            neighbors = new int[args.length - 6];
-            LOGGER.info("Starting...");
-            
-            parsePacket(acceptMessage());
-            
-            
-            
-            /*
-            FileHandler fh;
-            try {
-                fh = new FileHandler(System.getProperty("user.dir")+"/logs/"+Node.class.getName()+"_"+id+"_"+host+"-"+port+".log");
-                LOGGER.addHandler(fh);
-                SimpleFormatter frmt = new SimpleFormatter();
-                fh.setFormatter(frmt);
-            } catch (IOException ex) {
-                Logger.getLogger(Node.class.getName()).log(Level.SEVERE, ex.getMessage(), ex);
-            } catch (SecurityException ex) {
-                Logger.getLogger(Node.class.getName()).log(Level.SEVERE, ex.getMessage(), ex);
-            }
-*/            
-            
+                                   
             /* Get Adjacent List */
-/*            for(int i = 1; i < args.length; i++)
+            for(int i = 1; i < args.length; i++)
             {
                 parts = args[i].split(":");
                                 
                 Conn.Child c = new Conn.Child();
                 c.host = parts[1];
                 c.port = Integer.parseInt(parts[2]);
-                
-                children.put(Integer.parseInt(parts[0]), c);
-                incoming.put(Integer.parseInt(parts[0]), false);
-                outgoing.put(Integer.parseInt(parts[0]), false);
+                adjacent.put(Integer.parseInt(args[0]), c); //Adjacent Nodes...
             }
-*/            
-            /* Create New Node */
-            //Node node = new Node(id, host, port);
-        } 
+        }
         else 
         {
-/*            Values v1 = new Values(1L, 10);
-            Values v2 = new Values(2L, 20);
-            Values v3 = new Values(3L, 30);
-            
-            data.put("A", v1);
-            data.put("B", v2);
-            data.put("C", v3);
-           
-            Values v = data.get("A");
-            System.out.println(v.VALUE);
-            
-            v.VALUE = 5000;
-            
-            Values va = data.get("A");
-            System.out.println(va.VALUE);
-*/                    
+//            Values v1 = new Values(1L, 10);
+//            Values v2 = new Values(2L, 20);
+//            Values v3 = new Values(3L, 30);
+//            
+//            data.put("A", v1);
+//            data.put("B", v2);
+//            data.put("C", v3);
+//           
+//            Values v = data.get("A");
+//            System.out.println(v.VALUE);
+//            
+//            v.VALUE = 5000;
+//            
+//            Values va = data.get("A");
+//            System.out.println(va.VALUE);
             System.exit(1);     //Something Went Wrong...
+        }    
+           
+        LOGGER = Logger.getLogger(Node.class.getName()+"_"+id+"_"+host+"-"+port);
+        
+        /* LOG FILES MUST BE UNIQUE PER NODE INSTANCE */
+        FileHandler fh;
+        try {
+            fh = new FileHandler(System.getProperty("user.dir")+"/logs/"+Node.class.getName()+"_"+id+"_"+host+"-"+port+".log");
+            LOGGER.addHandler(fh);
+            SimpleFormatter frmt = new SimpleFormatter();
+            fh.setFormatter(frmt);
+        } catch (IOException ex) {
+            LOGGER.log(Level.SEVERE, ex.getMessage(), ex);
+        } catch (SecurityException ex) {
+            LOGGER.log(Level.SEVERE, ex.getMessage(), ex);
         }
+            
+        while(!shutdown) {
+            parsePacket(acceptMessage());
+        }
+
         
         System.exit(0);
     }
-    
     
     public static class Values {
         public long TIME;
@@ -328,7 +299,7 @@ public class Node {
                     for (int i = 0; i < keys.length; i++)
                     {
                         Values v = updateQueue.get(keys[i]);
-                        sendPacket(keys[i], v);
+                        sendPacket(v);
                         v.COUNT++;
                         
                         if (v.COUNT == k)
--- src/main/java/com/distributed/network/PacketHelper.java
+++ src/main/java/com/distributed/network/PacketHelper.java
@@ -8,6 +8,7 @@ import main.java.com.distributed.Node;
  */
 public class PacketHelper {
     public static final int MESSAGE              = 1;
+    public static final int NODE_COMPLETE        = 2;
     
     public static final int INIT_SHUTDOWN        = 30;
     public static final int INIT_INITIATOR       = 31;
@@ -17,11 +18,10 @@ public class PacketHelper {
     /**
      * Gets a Packet containing only a message type.
      * @param type of message.
-     * @param id of node.
-     * @param entry to be updated
+     * @param value
      * @return 
      */
-    public static Packet getPacket(int type, Node.Values value)
+    public static Packet getPacket(int type, int id, Node.Values value)
     {
         Packet p = new Packet();
         p.type = type;
