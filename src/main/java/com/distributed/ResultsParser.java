5d42d4f8d762087c336aa04b9e1eefa6942703e5
egeis <rcoan@callutheran.edu>
2015-02-21 20:32:24 -0800

    Removed Testing text file Adding Results Parser.

---
+++ src/main/java/com/distributed/ResultsParser.java
@@ -0,0 +1,16 @@
+package main.java.com.distributed;
+
+/**
+ *
+ * @author Richard Coan
+ */
+public class ResultsParser {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
